8f4d1c592acd6f6f364aaf7053e830686a1c91f8
Norbert <cross@linux.pl>
2021-01-31 23:13:08 +0100

    Change PostPreviewSkeleton elements backgrounds to gray-200

--- frontend/src/components/PostPreviewSkeleton.jsx
+++ frontend/src/components/PostPreviewSkeleton.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 function PostPreviewSkeleton() {
   return (
     <div className="bg-white py-6 px-6 my-6 md:my-12 shadow-md border border-gray-200 rounded-md">
-      <div className="bg-gray-300 animate-pulse w-1/2 p-4 my-1" />
+      <div className="bg-gray-200 animate-pulse w-1/2 p-4 my-1" />
       <div className="flex justify-between">
-        <div className="bg-gray-300 animate-pulse w-1/4 p-2" />
-        <div className="bg-gray-300 animate-pulse w-1/4 p-2" />
+        <div className="bg-gray-200 animate-pulse w-1/4 p-2" />
+        <div className="bg-gray-200 animate-pulse w-1/4 p-2" />
       </div>
-      <div className="bg-gray-300 animate-pulse w-full p-16 my-4" />
-      <div className="bg-gray-300 animate-pulse w-2/12 p-2" />
+      <div className="bg-gray-200 animate-pulse w-full p-16 my-4" />
+      <div className="bg-gray-200 animate-pulse w-2/12 p-2" />
     </div>
   );
 }
